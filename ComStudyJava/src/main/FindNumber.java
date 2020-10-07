ad010e994e5e476788b31a7092a0ddb119915396
202000358075 <2548653109@qq.com>
2020-10-07 11:30:10 +0800

    all

---
+++ ComStudyJava/src/main/FindNumber.java
@@ -0,0 +1,72 @@
+package main;
+
+public class FindNumber {
+	int sum=0;
+	float average=0;
+	int max=0;
+	int min=0;
+	int cur=0;
+	float sup=0;
+	float jige=0;
+	
+	public float ave(int a[]) 
+	{ 
+		for(int i=0;i<a.length;i++)
+			sum+=a[i];
+	    average=sum/a.length;
+		return average;
+		
+	}
+	public int max_num(int a[]) 
+	{ 
+	   // a.sore();
+	    max=a[a.length-1];
+		return max;
+		
+	}
+	public int min_num(int a[]) 
+	{ 
+		//min=a[0];
+		return min;
+		
+	}
+	public float max(int a[]) 
+	{ 
+		//获取排序后的数组
+		int b=a.length/2;
+		if(a.length%2==0)
+		{
+			cur=(a[b]+a[b+1])/2;
+		}else {
+		    cur=a[b];
+		}
+		
+		return cur;
+		
+	}
+	public float ji_number(int a[]) 
+	{ 
+		//获取排序后的数组
+		int ji_sum=0;
+	   for(int i=0;i<a.length;i++) {
+		  if(a[i]<60) {
+			  ji_sum+=a[i];  
+		  }
+	  }
+		sup=ji_sum/a.length;
+		
+		return jige;
+	}	
+		public float sup_num(int a[]) 
+		{ 
+			//获取排序后的数组
+		  int sup_sum=0;
+		  for(int i=0;i<a.length;i++) {
+			  if(a[i]>+90) {
+				  sup_sum+=a[i];  
+			  }
+		  }
+			sup=sup_sum/a.length;
+			return sup;
+		}		
+}
---
+++ ComStudyJava/src/main/MainTask.java
@@ -0,0 +1,26 @@
+package main;
+
+import java.util.ArrayList;
+
+public class MainTask {
+	
+	//public static int a[]= {12,23,2,66,34,57,89,90,78,89,99,100};
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	int a[]= {12,23,2,66,34,57,89,90,78,89,99,100};
+	
+	
+
+    FindNumber fn=new FindNumber();
+    System.out.println("平均分："+fn.ave(a));
+    System.out.println("最大值："+fn.max_num(a));
+    System.out.println("最小值："+fn.min_num(a));
+    System.out.println("中值"+fn.max(a));
+    System.out.println("及格率："+fn.ji_number(a));
+    System.out.println("优秀率："+fn.sup_num(a));
+    
+    
+    
+	}
+
+}

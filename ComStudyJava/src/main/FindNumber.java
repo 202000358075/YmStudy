af63438562ea3b607978050b45048b8a439d329f
202000358075 <2548653109@qq.com>
2020-10-07 15:24:45 +0800

    final modified version

--- ComStudyJava/src/main/FindNumber.java
+++ ComStudyJava/src/main/FindNumber.java
@@ -1,7 +1,7 @@
 package main;
 
 public class FindNumber {
-	int sum=0;
+	float sum=0;
 	float average=0;
 	int max=0;
 	int min=0;
@@ -19,26 +19,27 @@ public class FindNumber {
 	}
 	public int max_num(int a[]) 
 	{ 
-	   // a.sore();
+	  
 	    max=a[a.length-1];
 		return max;
 		
 	}
 	public int min_num(int a[]) 
 	{ 
-		//min=a[0];
+		min=a[0];
 		return min;
 		
 	}
-	public float max(int a[]) 
+	public float cur_num(int a[]) 
 	{ 
 		//获取排序后的数组
 		int b=a.length/2;
 		if(a.length%2==0)
 		{
-			cur=(a[b]+a[b+1])/2;
+			cur=(a[b]+a[b-1])/2;
+			
 		}else {
-		    cur=a[b];
+		    cur=a[b-1];
 		}
 		
 		return cur;
@@ -47,23 +48,23 @@ public class FindNumber {
 	public float ji_number(int a[]) 
 	{ 
 		//获取排序后的数组
-		int ji_sum=0;
+		float ji_sum=0;
 	   for(int i=0;i<a.length;i++) {
-		  if(a[i]<60) {
-			  ji_sum+=a[i];  
+		  if(a[i]>=60) {
+			  ji_sum=ji_sum+1;  
 		  }
 	  }
-		sup=ji_sum/a.length;
+	   jige=ji_sum/a.length;
 		
 		return jige;
 	}	
 		public float sup_num(int a[]) 
 		{ 
 			//获取排序后的数组
-		  int sup_sum=0;
+		  float sup_sum=0;
 		  for(int i=0;i<a.length;i++) {
-			  if(a[i]>+90) {
-				  sup_sum+=a[i];  
+			  if(a[i]>=90) {
+				  sup_sum+=1;  
 			  }
 		  }
 			sup=sup_sum/a.length;
--- ComStudyJava/src/main/MainTask.java
+++ ComStudyJava/src/main/MainTask.java
@@ -2,19 +2,20 @@ package main;
 
 public class MainTask {
 	
-	//public static int a[]= {12,23,2,66,34,57,89,90,78,89,99,100};
+	//public static int a[]= {12,23,2,66,34,57,87,90,78,89,99,100};
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	int a[]= {12,23,2,66,34,57,89,90,78,89,99,100};
+	int a[]= {12,23,2,66,34,57,89,90,78,87,99,100};
+
 	SortClass s=new SortClass();
 	s.sort(a);
-	
+	System.out.println();
 
     FindNumber fn=new FindNumber();
     System.out.println("平均分："+fn.ave(a));
     System.out.println("最大值："+fn.max_num(a));
     System.out.println("最小值："+fn.min_num(a));
-    System.out.println("中值"+fn.max(a));
+    System.out.println("中值:"+fn.cur_num(a));
     System.out.println("及格率："+fn.ji_number(a));
     System.out.println("优秀率："+fn.sup_num(a));
     

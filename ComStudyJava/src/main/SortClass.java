7970cc9b4716b274de0ef55a209299415152160e
202000358075 <2548653109@qq.com>
2020-10-07 15:34:33 +0800

    no use

--- ComStudyJava/src/main/SortClass.java
+++ ComStudyJava/src/main/SortClass.java
@@ -3,29 +3,26 @@ package main;
 import java.util.Arrays;
 
 public class SortClass {
-	
-	
-	public void sort(int arr[] ) {
-		int i,j,k;
-				int n=arr.length;
-		for(i=0;i<arr.length;i++) {
-			for(j=0;j<arr.length-1-i;j++) {
-				if(arr[j]>arr[j+1])
-				{
-					k=arr[j];
-					arr[j]=arr[j+1];
-					arr[j+1]=k;
+
+	public void sort(int arr[]) {
+		int i, j, k;
+		int n = arr.length;
+		for (i = 0; i < arr.length; i++) {
+			for (j = 0; j < arr.length - 1 - i; j++) {
+				if (arr[j] > arr[j + 1]) {
+					k = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = k;
 				}
 			}
 		}
-    	
-		System.out.println("排序结果是：");
-		for (int t=0;t<arr.length ;t++) {
-			System.out.print(" "+arr[t]);
-		}
-	
+
 		
-	
+		  System.out.println("排序结果是：");
+		  for (int t=0;t<arr.length ;t++) {
+		  System.out.print(" "+arr[t]); }
+		 
+
 	}
 
 }

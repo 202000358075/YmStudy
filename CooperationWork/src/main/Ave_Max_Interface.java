27235f66b18075825fc972f344660df481b71575
202000358040 <Administrator@USER-20181004VU>
2020-10-17 17:50:42 +0800

    liang

--- CooperationWork/src/main/Ave_Max_Interface.java
+++ CooperationWork/src/main/Ave_Max_Interface.java
@@ -1,5 +1,9 @@
 package main;
 
+import java.util.ArrayList;
+
 public interface Ave_Max_Interface {
+	public abstract float[] getAve(ArrayList<Person> stus);
+	public abstract float[] getMax(ArrayList<Person> stus);
 
 }
